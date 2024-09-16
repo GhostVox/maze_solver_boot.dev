a1984f773a68f5c61c9d289ab79adbe006c15fb4
Brent-the-carpenter <beh337@gmail.com>
2024-09-15 21:59:37 -0400

    I have added the line and point classes. I also added draw method to window.

--- graphics.py
+++ graphics.py
@@ -1,11 +1,32 @@
 from tkinter import Tk, Canvas , BOTH
 
+class Point():
+    def __init__(self,x:int , y:int) -> None:
+        self.x = x
+        self.y = y 
+    
+
+class Line():
+    def __init__(self, point1:Point , point2:Point) -> None:
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas:Canvas , fill_color:str):
+        canvas.create_line(self.point1.x,
+                           self.point1.y ,
+                           self.point2.x,
+                           self.point2.y,
+                           fill=fill_color,
+                           width= 2
+                           )
+
+        
 class Window():
     def __init__(self , width , height):
         self.__root = Tk()
         self.__root.title("Maze solver")
-        self.canvas = Canvas(width= width , height= height , background="white") 
-        self.canvas.pack()
+        self.__canvas = Canvas(self.__root , width= width , height= height , background="white") 
+        self.__canvas.pack(fill=BOTH , expand=1)
         self.running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
         
@@ -17,13 +38,13 @@ class Window():
         self.running = True
         while self.running:
             self.redraw()
-                    
+        print("window closed...")        
     def close(self):
         self.running = False
-        
-
+        self.__root.destroy()
     
-        
+    def draw_line(self , line:Line , fill_color:str="black"):
+        line.draw(self.__canvas , fill_color)
 
 
 
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from window_class import Window
+from graphics import Window, Point,Line
  
  
 def main():
     win = Window(800 , 600)
+    point_a = Point(3 ,5)
+    point_b = Point(10 ,10)
+    point_c = Point(400 ,400)
+    point_d = Point(500 ,500)
+
+    line_a_b = Line(point_a,point_b)
+    line_c_d = Line(point_c,point_d)
+    win.draw_line(line_a_b,"black")
+    win.draw_line(line_c_d , "red")
     win.wait_for_close()
 
 
